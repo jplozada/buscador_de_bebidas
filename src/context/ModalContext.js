63443562cacbd3091dbb8c0b54564af97cb91ad0
jplozada <juxnpx@gmail.com>
2020-10-08 16:50:46 -0400

    getting details of cocktails from thecocktaildb.com API and saving it on the State

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -8,6 +8,18 @@ const ModalProvider = (props) => {
 
     // State del provider
     const [idreceta, guardarIdReceta] = useState(null);
+    const [receta, guardarReceta] = useState({});
+
+    // Una vez que tenemos una receta, llamar a la api
+    useEffect(() => {
+        const obtenerReceta = async () => {
+            if(!idreceta) return;
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idreceta}`;
+            const resultado = await axios.get(url);
+            guardarReceta(resultado.data.drinks[0]);
+        }
+        obtenerReceta();
+    }, [idreceta])
 
     return (  
         <ModalContext.Provider
