dd05adfd912266bfd93a9fc094b66b316c25ba4e
jplozada <juxnpx@gmail.com>
2020-10-10 16:25:38 -0400

    url from RecetasContext fixed

--- src/context/RecetasContext.js
+++ src/context/RecetasContext.js
@@ -16,7 +16,7 @@ const RecetasProvider = (props) => {
     useEffect(() => {
         if(consultar){
             const obtenerRecetas = async () => {
-                const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${nombre}&?c=${categoria}`;
+                const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${nombre}&c=${categoria}`;
                 console.log(url);
                 const resultado = await axios.get(url);
                 //console.log(resultado.data.drinks);
